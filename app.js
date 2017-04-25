5f39439ff37fe9d7da698c9da8a59dd748c01b3b
nhwk <NH@OBSCatss-MacBook-Pro.local>
2017-04-25 10:47:40 +0100

    image updates

--- app.js
+++ app.js
@@ -601,7 +601,7 @@ function getCardsAttachments(session) {
             .title("Diet Tip: How to cut down on Sugar")
             .subtitle("Reducing your sugar intake will reduce your DHA by 0.5 years")
             .text("From the age of 11 we should have no more than 30g of added sugar (about 7 sugar cubes) in our diet every day.")
-            .image(builder.CardImage.create(session, "https://photos-6.dropbox.com/t/2/AABBhS94DJ1nJgzThLIJ9i5P7jHYHLz9TzQZm0CS5lO6Cw/12/315907919/png/32x32/1/_/1/2/Sugar.png/EPyl47cCGCkgBygH/5tPzPqo2CUJdvFKF1TNsDM4Zwz3s4vGd3q-xS_FoKmg?size=1280x960&size_mode=3"))
+            .image(builder.CardImage.create(session, "https://photos-6.dropbox.com/t/2/AAA_DVVdmfuR631Gp87mMAtVDjw1QyvwXozV6Rpvhp-SCA/12/315907919/jpeg/32x32/1/_/1/2/SugarThumbnail.jpg/EPyl47cCGC0gBygH/7NfMZ_3uXyAuokUIvDyebJyowfsTHL-0qdh4dozFLJc?size=32x32&size_mode=5"))
             .media([
                { url: "https://dl.dropboxusercontent.com/content_link/EOsSabT9XBQoj2cuipbjNSK7MnDRzzlCl7YiyjZ9vLaocNCqENuvJnlzjXf78a5y/file?dl=0&duc_id=HsladKdKcqeYUCgpkt9xn3wmaF3Eb7v96lya796wkoCUwhe1Xc8ItobWY8NRukQr&raw=1&size=1280x960&size_mode=3" }
             ])
@@ -614,7 +614,7 @@ function getCardsAttachments(session) {
 // Customer Data Receipt, ultimately for review and editing
 bot.dialog('/customerReceipt', [
     function (session) {
-        session.send("Here is a quick overview of the data you've entered");
+        session.send("Here is a quick overview of the data you've entered");      
         var msg = new builder.Message(session)
             .attachments([
                 new builder.ReceiptCard(session)
